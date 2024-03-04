3c4d015f183beaa11432b1d3819ea93df42af7ee
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-03-04 07:07:27 +0200

    init commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.controller.AppController;
+
+public class Main {
+
+    //    Starts the  app controller
+    public static void main(String[] args) {
+        AppController.Processing();
+    }
+}
---
+++ src/app/controller/AppController.java
@@ -0,0 +1,7 @@
+package app.controller;
+
+public class AppController {
+    public static void Processing() {
+        System.out.println("Hello World");
+    }
+}
